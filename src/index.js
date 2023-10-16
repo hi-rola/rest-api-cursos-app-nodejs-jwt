87490bdee08766d8468137129f28b693ff04a23a
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-10-16 12:03:03 -0600

    Se agrega ruta de acceso para el controlador de cursos

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import { PORT } from "./config.js";
 
 import authRoutes from "./routes/auth.routes.js";
 
+import usuariosRoutes from "./routes/usuarios.routes.js";
+import cursosRouter from "./routes/cursos.routes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api", usuariosRoutes);
+app.use("/api", cursosRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({
